63f53f9435dbdf8953b09b6d14e2ca8e1b9f1526
jaceleey <87891589+jaceleey@users.noreply.github.com>
2021-07-24 16:01:36 +0900

    Create grade calculator.py

---
+++ grade calculator.py
@@ -0,0 +1,9 @@
+print("*** grade calculator ***)
+# user input
+score= int(input("what your score?"))
+if score >=90:
+      print("A!")
+elif score >=70:
+    print("B")
+elif score >=50:
+    print("C")
