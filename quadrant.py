63991846f2dc9636f264557d2ab5431f4335a62b
jaceleey <87891589+jaceleey@users.noreply.github.com>
2021-07-31 16:26:08 +0900

    Create quadrant.py

---
+++ quadrant.py
@@ -0,0 +1,10 @@
+num=int(input("enter a number:"))
+print(num)
+if X > 0 and y > 0:
+    print("quadrant:1")
+elif x > 0 and y < 0:
+    print("quadrant 4")
+elif x > 0 and y < 0:
+    print("quadrant 3")
+else:
+    print("quadrant 2")
